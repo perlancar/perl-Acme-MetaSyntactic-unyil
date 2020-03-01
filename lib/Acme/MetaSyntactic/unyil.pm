cd832b0fc057037d07e52006882f5a00cce4cc7d
perlancar (on pc-home) <perlancar@gmail.com>
2020-03-01 09:03:27 +0700

    Initial commit

---
+++ lib/Acme/MetaSyntactic/unyil.pm
@@ -0,0 +1,43 @@
+package Acme::MetaSyntactic::unyil;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use parent qw(Acme::MetaSyntactic::MultiList);
+__PACKAGE__->init;
+
+1;
+# ABSTRACT: The Unyil theme
+
+=head1 SYNOPSIS
+
+ % perl -MAcme::MetaSyntactic=unyil -le 'print metaname'
+ ucrit
+
+ % metasyn unyil | shuf | head -n2
+ kinoy
+ ogah
+
+
+=head1 DESCRIPTION
+
+Si Unyil is a 1980's Indonesian children TV series.
+
+
+=head1 SEE ALSO
+
+L<https://en.wikipedia.org/wiki/Si_Unyil>
+
+L<Acme::MetaSyntactic>
+
+=cut
+
+__DATA__
+# default
+main
+# names main
+unyil ucrit usro kinoy meilani tina bunbun ableh ogah cuplis endut bariah jontor joni tina tineke
+# names extra
+raden lurah hansip
